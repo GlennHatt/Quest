4ff7fb13719b24b4f4876474bb86a6a11b1ff296
ryan8440 <ryan8440@gmail.com>
2016-02-09 21:56:38 -0600

    added logout button and user greeting

--- Quest/QuestWebApp/Master-Pages/admin.master.designer.cs
+++ Quest/QuestWebApp/Master-Pages/admin.master.designer.cs
@@ -49,6 +49,15 @@ namespace QuestWebApp.Master_Pages {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder adminBody;
         
         /// <summary>
+        /// txtAdminGreeting control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txtAdminGreeting;
+        
+        /// <summary>
         /// adminPageSpecificJS control.
         /// </summary>
         /// <remarks>
--- Quest/QuestWebApp/Master-Pages/teacher.master.designer.cs
+++ Quest/QuestWebApp/Master-Pages/teacher.master.designer.cs
@@ -49,6 +49,15 @@ namespace QuestWebApp.Master_Pages {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder teacherBody;
         
         /// <summary>
+        /// txtTeacherGreeting control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txtTeacherGreeting;
+        
+        /// <summary>
         /// teacherPageSpecificJS control.
         /// </summary>
         /// <remarks>
