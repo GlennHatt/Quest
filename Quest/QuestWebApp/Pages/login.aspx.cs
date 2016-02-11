b49e849a583ba214d99b046aee9599a1d7236be5
youseman <yousefkho1@gmail.com>
2016-02-11 13:54:12 -0600

    Login Page First

---
+++ Quest/QuestWebApp/Pages/login.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QuestWebApp.Pages
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginButton_Click(object sender, EventArgs e)
+        {
+            if (username.Text == "yousef" && password.Text == "khury")
+                answer.Text = "Login";
+            else
+                answer.Text = "Incorrect username and/or password";
+        }
+    }
+}
