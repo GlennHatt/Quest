c8fbc970e3447ec803dbff36005d4ffe99133222
youseman <yousefkho1@gmail.com>
2016-03-01 14:19:29 -0600

    Session variable for login page as well as hash algorithm.

--- Quest/QuestWebApp/Pages/loginTest.aspx.cs
+++ Quest/QuestWebApp/Pages/loginTest.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,7 +20,7 @@ namespace QuestWebApp.Pages
         {
             // All passwords are placholders that should be replaces with the passwords stored in the database
 
-            int classification;
+            Session["classification"] = "X";
 
             switch (username.Text)
             {
@@ -26,41 +28,52 @@ namespace QuestWebApp.Pages
                     if (password.Text == "password")
                     {
                         Response.Redirect("adminDashboard.aspx");
-                        classification = 1; // Replace with primary key for admin
+                        Session["classification"] = 'A';
                     }
                     else
-                    {
                         answer.Text = "Incorrect username and/or password";
-                    }
                     break;
                 case "teacher": // Placeholder for teacher username
-                    if (password.Text == "khury")
+                    if (password.Text == "password")
                     {
                         Response.Redirect("TeacherDashboard.aspx");
-                        classification = 2; // Replace with primary key for teacher
+                        Session["classification"] = 'T';
                     }
                     else
-                    {
                         answer.Text = "Incorrect username and/or password";
-                    }
                     break;
                 case "student": // Placeholder for student username
-                    if (password.Text == "khury")
+                    if (password.Text == "password")
                     {
                         Response.Redirect("StudentDashboard.aspx");
-                        classification = 3;  // Replace with primary key for the student
+                        Session["classification"] = 'S';
                     }
                     else
-                    {
                         answer.Text = "Incorrect username and/or password";
-                    }
                     break;
                 default:
                     answer.Text = "Incorrect username and/or password";
                     break;
             }
         }
-             protected void discoverButton_Click(object sender, EventArgs e)
+
+        public string CalculateHash(string passwordInput) // Hashes the string that is passed into it
+        {
+            MD5 hashed_algorithm = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(passwordInput);
+            byte[] hash = hashed_algorithm.ComputeHash(inputBytes);
+
+            StringBuilder hashedValue = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                hashedValue.Append(hash[i].ToString("X2"));
+            }
+
+            return hashedValue.ToString();
+        }
+
+        protected void discoverButton_Click(object sender, EventArgs e)
         {
             Response.Redirect("AboutUsPage.aspx");
         }
