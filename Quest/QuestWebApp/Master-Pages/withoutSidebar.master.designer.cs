c76943d213ec19487e940b661b21b73c7a5b95fb
ryan8440 <ryan8440@gmail.com>
2016-03-07 20:01:10 -0600

    fixed withoutsidebar master page and taking test mester page (scrolling)

--- Quest/QuestWebApp/Master-Pages/withoutSidebar.master.designer.cs
+++ Quest/QuestWebApp/Master-Pages/withoutSidebar.master.designer.cs
@@ -49,6 +49,15 @@ namespace QuestWebApp.Master_Pages {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder withoutSidebarHeadBodyContent;
         
         /// <summary>
+        /// withoutSidebarBodyContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder withoutSidebarBodyContent;
+        
+        /// <summary>
         /// withoutSidebarPageSpecificJS control.
         /// </summary>
         /// <remarks>
