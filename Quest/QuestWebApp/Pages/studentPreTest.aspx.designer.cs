70862290ccc8c12cb7bf0df73c3add85b3cd718d
122928 <llewis1416@students.pcci.edu>
2016-03-08 14:21:28 -0600

    pledge done

--- Quest/QuestWebApp/Pages/studentPreTest.aspx.designer.cs
+++ Quest/QuestWebApp/Pages/studentPreTest.aspx.designer.cs
@@ -11,5 +11,23 @@ namespace QuestWebApp.Pages {
     
     
     public partial class studentPreTest {
+        
+        /// <summary>
+        /// btnNotReady control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnNotReady;
+        
+        /// <summary>
+        /// btnReady control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnReady;
     }
 }
