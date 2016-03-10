52bbd140113e702354ec688c6641e319ea6a7736
jesseupchurch <jesse.l.upchurch@gmail.com>
2016-03-10 15:23:11 -0600

    I think the insertion function for a graded question is working.

--- Quest/QuestWebApp/Pages/takingTestTest.aspx.cs
+++ Quest/QuestWebApp/Pages/takingTestTest.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OracleClient;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -20,14 +22,37 @@ namespace QuestWebApp.Pages
 
         protected void btnSubmitTest_Click(object sender, EventArgs e)
         {
+            string TFAnswer;
+
             foreach(ListViewItem question in lvTest.Items)
             {
-                
-               /* txtTestAnswer.Text += "/" + ((Label)question.FindControl("Label1")).Text + ", ";
                 if (((RadioButton)question.FindControl("rdbTrue")).Checked == true)
-                    txtTestAnswer.Text += "T";
+                    TFAnswer = "T";
                 else
-                    txtTestAnswer.Text += "F"; */     
+                    TFAnswer = "F";
+
+                OracleCommand cmdAddTFQuestion = new OracleCommand(@"
+
+BEGIN
+   questions_true_false.grade_question(
+    p_TestTakenID   => :p_TestTakenID,
+    p_QuestionID    => :p_QuestionID,
+    p_StudentAnswer => :p_Answer);
+END;",
+             new OracleConnection(ConfigurationManager.ConnectionStrings["ProductionDB"].ConnectionString));
+                cmdAddTFQuestion.Parameters.AddWithValue("p_TestTakenID", 1);
+                cmdAddTFQuestion.Parameters.AddWithValue("p_QuestionID", ((Label)question.FindControl("Label1")).Text);
+                cmdAddTFQuestion.Parameters.AddWithValue("p_Answer", TFAnswer);
+
+                cmdAddTFQuestion.Connection.Open();
+                cmdAddTFQuestion.ExecuteNonQuery();
+
+                cmdAddTFQuestion.Connection.Close();
+                /* txtTestAnswer.Text += "/" + ((Label)question.FindControl("Label1")).Text + ", ";
+                 if (((RadioButton)question.FindControl("rdbTrue")).Checked == true)
+                     txtTestAnswer.Text += "T";
+                 else
+                     txtTestAnswer.Text += "F"; */
             }
         }
 
