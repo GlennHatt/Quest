198677085e3e476a9f04e2aec672bd21cb05f5ad
ryan8440 <ryan8440@gmail.com>
2016-03-14 13:40:21 -0500

    added misc.js

---
+++ Quest/QuestWebApp/Assets/JS/misc.js
@@ -0,0 +1,16 @@
+﻿function changeUserCardSize(cardID, SmButtonID, LrgButtonID) {
+
+    //document.writeln("test");
+    if (document.getElementById(LrgButtonID).disabled == false) {
+        document.getElementById(cardID).className = "mdl-cell mdl-cell--12-col";
+        document.getElementById(LrgButtonID).disabled = true;
+        document.getElementById(SmButtonID).disabled = false;
+    }
+    else {
+        document.getElementById(cardID).className = "mdl-cell mdl-cell--4-col";
+        document.getElementById(LrgButtonID).disabled = false;
+        document.getElementById(SmButtonID).disabled = true;
+    }
+
+    return false;
+}
