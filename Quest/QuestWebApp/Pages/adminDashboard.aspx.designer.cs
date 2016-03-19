fe04333dc6e68388ff4fd30aa00b668111e9e849
jesseupchurch <jesse.l.upchurch@gmail.com>
2016-03-19 11:21:31 -0500

    Working on insert

--- Quest/QuestWebApp/Pages/adminDashboard.aspx.designer.cs
+++ Quest/QuestWebApp/Pages/adminDashboard.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace QuestWebApp.Pages {
         protected global::System.Web.UI.WebControls.SqlDataSource sqlClass;
         
         /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
         /// lblWarning control.
         /// </summary>
         /// <remarks>
