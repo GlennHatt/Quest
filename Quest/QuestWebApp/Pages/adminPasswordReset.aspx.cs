4c8f19577b2ad26025138e7abe30b700e49caf4f
122928 <llewis1416@students.pcci.edu>
2016-04-08 19:31:22 -0500

    Admin password reset page

---
+++ Quest/QuestWebApp/Pages/adminPasswordReset.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QuestWebApp.Pages
+{
+    public partial class adminPasswordReset : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            cardUpdatePassword.Visible = false;
+        }
+
+        protected void ddlClassSelect_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+            cardUpdatePassword.Visible = true;
+        }
+    }
+}
