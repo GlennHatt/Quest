712981e68fc9dc11133701cf35653f0477711131
GlennHatt <GlennHatt@Gmail.com>
2016-04-14 13:48:12 -0500

    fixed some redirects

--- Quest/QuestWebApp/Pages/AboutUsPage.aspx.cs
+++ Quest/QuestWebApp/Pages/AboutUsPage.aspx.cs
@@ -16,7 +16,7 @@ namespace QuestWebApp.Pages
 
       protected void btnbackToLogin_Click(object sender, EventArgs e)
       {
-         Response.Redirect("http://localhost:52416/Pages/login.aspx");
+         Response.Redirect("~/Pages/login.aspx");
       }
    }
 }
