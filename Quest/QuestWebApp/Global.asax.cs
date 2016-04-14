c587053ed1c50f2849659a16fd3fef97442a0011
jesseupchurch <jesse.l.upchurch@gmail.com>
2016-04-14 13:17:37 -0500

    Deleted everything.

--- Quest/QuestWebApp/Global.asax.cs
+++ Quest/QuestWebApp/Global.asax.cs
@@ -13,8 +13,6 @@ namespace QuestWebApp
       void Application_Start(object sender, EventArgs e)
       {
          // Code that runs on application startup
-         AuthConfig.RegisterOpenAuth();
-         RouteConfig.RegisterRoutes(RouteTable.Routes);
       }
 
       void Application_End(object sender, EventArgs e)
