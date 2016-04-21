f7b2e0efd25962e220876136569916dcd0b17774
ryan8440 <ryan8440@gmail.com>
2016-04-21 16:45:58 -0500

    working on timer

--- Quest/QuestWebApp/Pages/studentTestTaking.aspx.designer.cs
+++ Quest/QuestWebApp/Pages/studentTestTaking.aspx.designer.cs
@@ -76,6 +76,15 @@ namespace QuestWebApp.Pages {
         protected global::System.Web.UI.WebControls.Label lblProgressBar;
         
         /// <summary>
+        /// lblTest control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTest;
+        
+        /// <summary>
         /// lstQuestions control.
         /// </summary>
         /// <remarks>
